c7431631c5d6a39abff3e73cef153f39b7a328a4
JonathanSilvaDev <jonsilva614@gmail.com>
2023-09-11 18:33:19 -0300

    js inicial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1')
+const color2 = document.querySelector('.color2')
 
+// console.log(css,color1,color2)
+
+color1.addEventListener('input', function() {
+    console.log(color1.value)
+} )
+
+color2.addEventListener('input', function() {
+    console.log(color2.value)
+})
