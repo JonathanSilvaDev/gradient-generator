82edc8e5b4ecbd323109c5de6b124f702e28798f
JonathanSilvaDev <jonsilva614@gmail.com>
2023-09-11 19:24:25 -0300

    configuração padrão terminada

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const css = document.querySelector('h3');
 const color1 = document.querySelector('.color1')
 const color2 = document.querySelector('.color2')
+const body = document.getElementById('gradient')
 
 // console.log(css,color1,color2)
 
-color1.addEventListener('input', function() {
-    console.log(color1.value)
-} )
+function changeColor() {
+    body.style.background = 
+    `Linear-gradient(to right,${color1.value}, ${color2.value})`
 
-color2.addEventListener('input', function() {
-    console.log(color2.value)
-})
+    css.textContent = body.style.background
+
+}
+
+
+color1.addEventListener('input', changeColor)
+
+color2.addEventListener('input', changeColor)
